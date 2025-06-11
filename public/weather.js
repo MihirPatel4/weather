d4b88d6edd3610a0368c16e88b9c154571857f03
Mihir Patel <mk04patel@gmail.com>
2025-06-11 12:36:00 -0500

    Made createForecast() strictly for creating forecast elements

--- public/weather.js
+++ public/weather.js
@@ -1,4 +1,6 @@
 const forecast = document.getElementById('forecast')
+const cityForm = document.getElementById('choose-city')
+const cityInput = document.getElementById('city-input')
 
 async function getWeatherData(q) {
     let response = await fetch(`/weather?q=${encodeURIComponent(q)}`)
@@ -6,15 +8,8 @@ async function getWeatherData(q) {
     return data
 }
 
-async function createForecast(q) {
-    let data = await getWeatherData(q)
-
-    document.getElementById('location').textContent = `${data.location.name}, ${data.location.region}, ${data.location.country}`
-    document.getElementById('temp').textContent = `Current temp: ${data.current.temp_f}°F / ${data.current.temp_c}°C`
-    document.getElementById('condition').textContent = data.current.condition.text
-    document.getElementById('last-updated').textContent = `Last updated ${data.current.last_updated}`
-
-    data.forecast.forecastday.forEach((day, index) => {
+function createForecast() {
+    for (let index = 0; index < 7; index++) {
         let dayContainer = document.createElement('div')
         dayContainer.id = `day-container${index}` //different id for each day so they can be edited later
         dayContainer.classList.add('day-container') //same class used for each day for styling purposes
@@ -22,14 +17,11 @@ async function createForecast(q) {
 
         let date = document.createElement('p')
         date.id = `date${index}`
-        date.textContent = day.date
         date.classList.add('day-info')
         dayContainer.appendChild(date)
 
         let lowTemp = document.createElement('p')
-        lowTemp.textContent = `Low: ${day.day.mintemp_f}°F / ${day.day.mintemp_c}°C`
         let highTemp = document.createElement('p')
-        highTemp.textContent = `High: ${day.day.maxtemp_f}°F / ${day.day.maxtemp_c}°C`
         lowTemp.id = `low-temp${index}`
         highTemp.id = `high-temp${index}`
         lowTemp.classList.add('day-info')
@@ -38,28 +30,16 @@ async function createForecast(q) {
         dayContainer.appendChild(highTemp)
 
         let condition = document.createElement('p')
-        condition.textContent = day.day.condition.text
         condition.id = `condition${index}`
         condition.classList.add('day-info')
         dayContainer.appendChild(condition)
 
         let conditionIcon = document.createElement('img')
-        conditionIcon.src = 'https:' + day.day.condition.icon
         conditionIcon.id = `condition-icon${index}`
         conditionIcon.classList.add('condition-icon')
         dayContainer.appendChild(conditionIcon)
-    })
+    }
 }
-
-createForecast(39465)
-
-const cityForm = document.getElementById('choose-city')
-const cityInput = document.getElementById('city-input')
-
-cityForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    newForecast(cityInput.value)
-})
 
 async function newForecast(q) {
     let data = await getWeatherData(q)
@@ -84,3 +64,13 @@ async function newForecast(q) {
         document.getElementById(`condition-icon${index}`).src = 'https:' + day.day.condition.icon
     })
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+    createForecast()
+    newForecast('Birmingham')
+})
+
+cityForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    newForecast(cityInput.value)
+})
